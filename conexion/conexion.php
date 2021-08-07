7490a4be611d92604ca7806752c0b68d9df89c3d
Ego <augurto.17@gmail.com>
2021-08-07 08:09:14 -0500

    parte dos carpetas
    asd

---
+++ conexion/conexion.php
@@ -0,0 +1,6 @@
+<?php
+
+	mysql_connect("localhost","edeproca_fix_test_user","fix_test_user");
+	mysql_select_db("edeproca_p");
+
+?>
---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+/*Datos de conexion a la base de datos*/
+define('DB_HOST', 'localhost');//DB_HOST:  generalmente suele ser "127.0.0.1"
+define('DB_USER', 'edeproca_fix_test_user');//Usuario de tu base de datos
+define('DB_PASS', 'fix_test_user');//Contraseña del usuario de la base de datos
+define('DB_NAME', 'edeproca_p');//Nombre de la base de datos
+ 
+
+?>
