7cad0c34d5ed4356da434d5c6c3916bd432b1a4e
Ego <augurto.17@gmail.com>
2021-08-13 06:47:01 -0500

    acceso de regerso a como estaba

--- ajax/acceso.php
+++ ajax/acceso.php
@@ -9,8 +9,7 @@ require_once ("../config/conexion.php");
 if(isset($_POST["username"]) && isset($_POST["password"])){
   $username = mysqli_real_escape_string($con, $_POST["username"]);
   $password = mysqli_real_escape_string($con,  sha1($_POST["password"]));
-  $pass = $_POST["password"];
-  $sql = "SELECT username,id,rol, codigo_proyecto FROM usuarios or miembros WHERE username='$username' or email='$username' AND password='$password' or  rand ='$pass' GROUP BY username";
+  $sql = "SELECT username,id,rol, codigo_proyecto FROM usuarios WHERE username='$username' AND password='$password' GROUP BY username";
   $result = mysqli_query($con, $sql);
   $num_row = mysqli_num_rows($result);
   if ($num_row == "1") {
