049d94c7e08e6b1bf6081e1d6908776cef2dccdb
Ego <augurto.17@gmail.com>
2021-08-14 07:17:57 -0500

    corrigiendo parte 2

--- ajax/nuevo_usuario_general.php
+++ ajax/nuevo_usuario_general.php
@@ -5,12 +5,26 @@ $usuario = $_POST["usuario"];
 $password = $_POST["pass"];
 $sha1 = sha1($password);
 
-$sql=" INSERT INTO usuarios (username , password, pass2) VALUES ('$usuario', '$sha1', '$sha1')";
-
-        if (mysqli_query($conn, $sql)) {
-            echo "Usuario Registrado";
-        } else {
-            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-        }
-        mysqli_close($conn);
+$servername = "localhost";
+$database = "u415020159_mantizb";
+$username = "u415020159_mantizb";
+$password = "Mantizb*#17";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+// Check connection
+if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
+}
+ 
+echo "Connected successfully";
+ 
+$sql = "INSERT INTO usuarios (username , password, rol,codigo_proyecto, pass2) VALUES ('$usuario', '$sha1','Inv Principal','null', '$sha1')";
+if (mysqli_query($conn, $sql)) {
+      echo "Usario registrado con exito :3";
+} else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+mysqli_close($conn);
 ?>
+
+
