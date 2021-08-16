d72ba877697efe386f17a6b0079ee77c174ae5bd
Ego <augurto.17@gmail.com>
2021-08-16 18:34:06 -0500

    probaremos directo a la bd

--- modal/seguimientos.php
+++ modal/seguimientos.php
@@ -18,16 +18,6 @@
       <div class="col-sm-12">
        <div class="form-group">
         <label for="exampleInputFile">Archivo</label>
-        <?php 
-        require_once ("../config/db.php");//Contiene las variables de configuracion para conectar a la base de datos
-        require_once ("../config/conexion.php");//Contiene funcion que conecta a la base de datos
-        $abc=$_SESSION["username"];
-
-        $ge=mysqli_query($con,"SELECT id FROM miembros WHERE nombre='".$abc."'");
-        $rws=mysqli_fetch_array($ge);
-         $entregables=$rw["id"];
-        
-        ?>
         <input type="text" value="<?php  echo $rws["id"];?>">
         <div class="input-group">
         <div class="custom-file">
@@ -40,9 +30,9 @@
         <div class="col-sm-12">
         <select name="nomb"  id="nomb" onkeyup="select();"  class="form-control" required>
         </select>
-        <input type="text" name="cd" placeholder="Nombre del seguimiento"  id="cd"  class="form-control">
-        <input type="text" name="cdd" placeholder="Nombre del seguimiento"  id="cdd"  class="form-control">
-        <input type="text" name="id" placeholder="Nombre del seguimiento"  id="id"  class="form-control">
+        <input type="hidden" name="cd" placeholder="Nombre del seguimiento"  id="cd"  class="form-control">
+        <input type="hidden" name="cdd" placeholder="Nombre del seguimiento"  id="cdd"  class="form-control">
+        <input type="hidden" name="id" placeholder="Nombre del seguimiento"  id="id"  class="form-control">
         </div>
         <div class="modal-footer">
           <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancelar</button>
