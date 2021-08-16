d16a48c9dd3388911abdce3989ca48bf68fd4358
Ego <augurto.17@gmail.com>
2021-08-16 18:56:23 -0500

    prueba con select

--- ajax/select.php
+++ ajax/select.php
@@ -3,6 +3,7 @@
 	require_once ("../config/conexion.php");//Contiene funcion que conecta a la base de datos
 	
  $cd=$_GET['cd'];
+ $habita=$_SESSION['usermane'];
   $p=mysqli_query($con,"select * from proyecto where id='".$cd."'");
 $rws=mysqli_fetch_array($p);
                       $codigo=$rws["codigo"];
@@ -10,6 +11,18 @@ $rws=mysqli_fetch_array($p);
 <option value="">--Seleccione el entregable--</option>
 					 <?php
 										$ent=mysqli_query($con,"select * from entregables where codigo_proyecto='".$codigo."'");
+										while ($rw=mysqli_fetch_array($ent)){
+											$id=$rw["id"];
+											$nombre=$rw["nombre"];
+											?>
+											<option value="<?php echo $id?>"><?php echo $nombre?></option>
+											<?php
+										}
+									?>
+
+<option value="">--Seleccione el entregable2--</option>
+<?php
+										$ent=mysqli_query($con,"select * from miembros where nombre='".$habita."'");
 										while ($rw=mysqli_fetch_array($ent)){
 											$id=$rw["id"];
 											$nombre=$rw["nombre"];
