50b794f94cf9826fab411dd0575b6c5aea80553b
Ego <augurto.17@gmail.com>
2021-08-18 09:39:39 -0500

    anteponiendo

--- ver_entregables.php
+++ ver_entregables.php
@@ -119,7 +119,7 @@
                   <div  align="center"><img src="img/file.png" width="50px" height="auto"></div>
                   <div align="center"><a href="entregables/<?php echo  $id_p  ; ?>/<?php echo  $documento; ?>" download="entregables/<?php echo  $id_p  ; ?>/<?php echo  $documento; ?>"><i class="fa fa-download"></i> <?php echo $documento; ?></a></div>
 
-                  <div align="center"><a href="<?php echo  $link  ; ?>/<?php echo  $link; ?>" download="<?php echo  $link  ; ?>/<?php echo  $link; ?>" target="_blank"><i class="fa fa-cloud-download" ></i><p>URL: </p>  <?php echo $link; ?></a></div>
+                  <div align="center"><p>URL: </p> <a href="<?php echo  $link  ; ?>/<?php echo  $link; ?>" download="<?php echo  $link  ; ?>/<?php echo  $link; ?>" target="_blank"><i class="fa fa-cloud-download" ></i>  <?php echo $link; ?></a></div>
 
                   
                   
