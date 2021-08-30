880ee2ae65a6b42b27226d2b4c39bc35f6d5a132
Ego <augurto.17@gmail.com>
2021-08-30 14:51:48 -0500

    title='Editar proyecto'  arregalndo

--- ajax/buscar_proyectos.php
+++ ajax/buscar_proyectos.php
@@ -206,8 +206,8 @@
                     <span class="text">Entregables</span>
                   </a>
 				<span class="pull-right">
-					<a href="#" class='btn btn-primary' data-toggle="modal" data-target="#editProyecto"  onclick="obtener_datos(<?php echo $id;?>);"><i class="fa fa-edit"></i></a>
-						<a href="#" class='btn btn-primary' title='Editar proyecto' onclick="eliminar('<?php echo $codigo_;?>');"><i class="fa fa-trash"></i></a> 
+					<a href="#" class='btn btn-primary' data-toggle="modal" data-target="#editProyecto" title='Editar proyecto'  onclick="obtener_datos(<?php echo $id;?>);"><i class="fa fa-edit"></i></a>
+						<a href="#" class='btn btn-primary' title='Borrar proyecto' onclick="eliminar('<?php echo $codigo_;?>');"><i class="fa fa-trash"></i></a> 
 					</span>
 					<p></p>
 				</td>
