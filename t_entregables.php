232ee1bc74929e0e03dad3f95b117a56afaef42f
Ego <augurto.17@gmail.com>
2021-08-30 14:03:56 -0500

    ocultando nombre

--- t_entregables.php
+++ t_entregables.php
@@ -79,7 +79,7 @@
         <!-- Begin Page Content -->
         <div class="container-fluid">
  <div class="d-sm-flex align-items-center justify-content-between mb-4">
-            <h1 class="h3 mb-0 text-gray-800"><b><?php echo $nombre; ?> </b></h1>
+        <!--     <h1 class="h3 mb-0 text-gray-800"><b><?php echo $nombre; ?> </b></h1> -->
           </div>
 
           <!-- DataTales Example -->
