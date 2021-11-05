6b00d50183341b3d35c2ffc858c4c1d61bff6ddd
Ego <augurto.17@gmail.com>
2021-11-05 08:25:37 -0500

    retoques editar obra y borrar

--- inventario_general.php
+++ inventario_general.php
@@ -64,7 +64,7 @@
         <!-- Begin Page Content -->
         <div class="container-fluid">
  <div class="d-sm-flex align-items-center justify-content-between mb-4">
-            <h1 class="h3 mb-0 text-gray-800"><b>Invnetario General </b></h1>
+            <h1 class="h3 mb-0 text-gray-800"><b>Inventario General </b></h1>
                <?php if($_SESSION['prol']=='administrador'   || $_SESSION['prol']=="Inv Principal"){?>
             <a href="#" class="d-sm-inline-block btn btn-sm btn-primary shadow-sm"   data-toggle="modal" data-target="#nuevoPrograma"><i class="fas fa-user fa-sm text-white-50"></i> Nueva Obra</a>
           <?php } ?>
