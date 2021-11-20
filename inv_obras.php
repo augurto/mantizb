8128ed99b339c1ff5b3c672b02d24f8ef9e979d9
Ego <augurto.17@gmail.com>
2021-11-20 03:49:59 -0500

    inv obras incorporando lineas faltantes

--- inv_obras.php
+++ inv_obras.php
@@ -1,9 +1,12 @@
 <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
        
 
-       <?php
-       session_start();
-
+       
+       <?php session_start();
+       if (!isset($_SESSION['id_user'])) {
+             header("location: login.php");
+         exit;
+       }
              /* Connect To Database*/
   require_once ("config/db.php");//Contiene las variables de configuracion para conectar a la base de datos
   require_once ("config/conexion.php");//Contiene funcion que conecta a la base de datos
