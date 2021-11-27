fc5d51bd44e76b362be927cd5c183f7214444461
Ego <augurto.17@gmail.com>
2021-11-27 05:51:45 -0500

    where id_obra =$id_obra falta quitarle el comentar

--- ajax/buscar_materiales_2.php
+++ ajax/buscar_materiales_2.php
@@ -67,7 +67,7 @@
                $total_pages = ceil($numrows/$per_page);
                $reload = './inventario_general.php';
                //main query to fetch the data
-               $sql="SELECT * FROM  $sTable /* where id_obra =$id_obra */ LIMIT $offset,$per_page";
+               $sql="SELECT * FROM  $sTable where id_obra =$id_obra LIMIT $offset,$per_page";
                $query = mysqli_query($con, $sql);
                //loop through fetched data
                if ($numrows>0){
