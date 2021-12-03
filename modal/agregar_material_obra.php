b8e9a2230145fd8abb2d6cb2c83739efd4295bcb
Ego <augurto@gmail.com>
2021-12-02 22:06:55 -0500

    agregando varios materiales

--- modal/agregar_material_obra.php
+++ modal/agregar_material_obra.php
@@ -4,11 +4,11 @@
 		require_once ("config/conexion.php");//Contiene funcion que conecta a la base de datos
 		// escaping, additionally removing everything that could be (html/javascript-) code
  ?>
-	 <div class="modal fade" id="nuevoMaterial" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	 <div class="modal fade" id="nuevoPrograma" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-cm" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel"><i class='fa fa-file'></i> Agregar Material a la Obra <?php echo $_GET['nom']?></h5>
+          <h5 class="modal-title" id="exampleModalLabel"><i class='fa fa-file'></i> Agregar Materiales a la OBra  <?php echo $_GET['nom']</h5>
           <button class="close" type="button" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">×</span>
           </button>
@@ -16,20 +16,40 @@
         <div class="modal-body">
 			<form class="form-horizontal" method="post" id="guardar_programa" name="guardar_programa">
 			<div id="resultados_ajax"></div>
-			 	<label>Nombre del material</label>
+			 	<label>Nombre de la obra</label>
 			  <div class="input-group mb-3">
 
                   <div class="input-group-prepend">
                     <span class="input-group-text"><span class="icon-books"></span></span>
                   </div>
-                  <label>Cantidad del material</label>
-                  <input type="text" class="form-control" name="obra" placeholder="Cantidad">
-                  <label>Fecha de compra</label>
-                  <input type="date" class="form-control" name="fecha">
-                  <label>Precio del material</label>
-                  <input type="text" class="form-control" name="obra" placeholder="Precio">
-                  
+                  <input type="text" class="form-control" name="obra" placeholder="Nombre del programa">
+                </div>
 
+                <label>Nombre de la obra</label>
+			  <div class="input-group mb-3">
+
+                  <div class="input-group-prepend">
+                    <span class="input-group-text"><span class="icon-books"></span></span>
+                  </div>
+                  <input type="text" class="form-control" name="obra" placeholder="Nombre del programa">
+                </div>
+
+                <label>Nombre de la obra</label>
+			  <div class="input-group mb-3">
+
+                  <div class="input-group-prepend">
+                    <span class="input-group-text"><span class="icon-books"></span></span>
+                  </div>
+                  <input type="text" class="form-control" name="obra" placeholder="Nombre del programa">
+                </div>
+
+                <label>Nombre de la obra</label>
+			  <div class="input-group mb-3">
+
+                  <div class="input-group-prepend">
+                    <span class="input-group-text"><span class="icon-books"></span></span>
+                  </div>
+                  <input type="text" class="form-control" name="obra" placeholder="Nombre del programa">
                 </div>
             </div>
 
