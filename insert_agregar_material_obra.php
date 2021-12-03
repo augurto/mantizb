91addad0f87ee486398e800500cab46b8b98eb13
Ego <augurto.17@gmail.com>
2021-12-03 06:18:48 -0500

    cambio de columnas

--- insert_agregar_material_obra.php
+++ insert_agregar_material_obra.php
@@ -13,14 +13,14 @@ $cantidad=$_POST["cantidad"];
 $id_obra=$_POST["id_obra"];
 $fecha=$_POST["fecha"];
 $fecha2=$_POST["fecha"];
-$usuario=$_POST["usuario"];
+$usuario="eguito";
 if (!$conn) {
       die("Connection failed: " . mysqli_connect_error());
 }
  
 echo "Connected successfully";
  
-$sql = "INSERT INTO materiales_obra (id_material, id_unidad, cantidad, id_obra, fecha, fecha_hoy, usuario) 
+$sql = "INSERT INTO materiales_obra (material, unidad, cantidad, id_obra, fecha, fecha_hoy, usuario) 
 VALUES ('$id_material', '$unidad', '$cantidad', '$id_obra','$fecha','$fecha2','$usuario')";
 if (mysqli_query($conn, $sql)) {
       echo "New record created successfully";
