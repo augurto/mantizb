63e9c838fd490b3af8b099f423485d8b907c3f14
Ego <augurto@gmail.com>
2021-12-02 22:09:57 -0500

    falta el cieerre ala php

--- modal/agregar_material_obra.php
+++ modal/agregar_material_obra.php
@@ -8,7 +8,7 @@
     <div class="modal-dialog modal-cm" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel"><i class='fa fa-file'></i> Agregar Materiales a la OBra  <?php echo $_GET['nom']</h5>
+          <h5 class="modal-title" id="exampleModalLabel"><i class='fa fa-file'></i> Agregar Materiales a la OBra  <?php echo $_GET['nom']?></h5>
           <button class="close" type="button" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">×</span>
           </button>
